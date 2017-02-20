629ba1f1334629ae0ce48f9297935ebdf2aeaf1c
Martin Blomberg <martin.blomberg@outlook.com>
2017-02-20 16:28:35 +0100

    Filtering of listed notes added
    Added filterining in in searchview to search for title of notes.

--- app/src/main/java/snowroller/notes/NotesList.java
+++ app/src/main/java/snowroller/notes/NotesList.java
@@ -1,13 +1,20 @@
 package snowroller.notes;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
+import android.widget.Toast;
 
 import snowroller.notes.models.NotesRepo;
 import snowroller.notes.viewmodels.NotesListViewModel;
@@ -15,12 +22,15 @@ import snowroller.notes.databinding.ActivityNotesListBinding;
 
 public class NotesList extends AppCompatActivity {
 
+    private NotesListViewModel viewmodel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityNotesListBinding binding = DataBindingUtil.setContentView(this,
                 R.layout.activity_notes_list);
-        binding.setViewmodel(new NotesListViewModel(new NotesRepo(this)));
+        viewmodel = new NotesListViewModel(this);
+        binding.setViewmodel(viewmodel);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -34,4 +44,29 @@ public class NotesList extends AppCompatActivity {
             }
         });
     }
-}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu_list, menu);
+
+        final SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                viewmodel.filter(s);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                if (s.length() == 0) {
+                    viewmodel.filter("");
+                }
+                else
+                    viewmodel.filter(s);
+                return true;
+            }
+        });
+        return true;
+    }
+ }
--- app/src/main/java/snowroller/notes/ViewModels/NotesListViewModel.java
+++ app/src/main/java/snowroller/notes/ViewModels/NotesListViewModel.java
@@ -6,41 +6,41 @@ import android.content.ContextWrapper;
 import android.database.ContentObserver;
 import android.database.Cursor;
 import android.databinding.BindingAdapter;
+import android.databinding.BindingMethod;
+import android.databinding.BindingMethods;
+import android.databinding.ObservableBoolean;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import snowroller.notes.models.Note;
-import snowroller.notes.models.NotesRepo;
 import snowroller.notes.models.provider.NotesContentProvider;
 
 /**
  * Created by Martin on 2017-02-09.
  */
 
-public class NotesListViewModel {
+public class NotesListViewModel implements LoaderManager.LoaderCallbacks<Cursor> {
 
-    private NotesRepo repo;
+    private Context context;
+    private CursorRecyclerAdapter adapter;
 
-    public NotesListViewModel(NotesRepo repo)
+    public NotesListViewModel(Context context)
     {
-        this.repo = repo;
+        this.context = context;
     }
 
-    public Cursor getNotes()
-    {
-        return repo.getNotes();
-    }
-
+    @Nullable
     private static AppCompatActivity getActivity(View view) {
         Context context = view.getContext();
         while (context instanceof ContextWrapper) {
@@ -51,13 +51,27 @@ public class NotesListViewModel {
         }
         return null;
     }
+    @Nullable
+    private static AppCompatActivity getActivity(Context context) {
+            if (context instanceof Activity) {
+                return (AppCompatActivity)context;
+            }
+        while (context instanceof ContextWrapper) {
+            if (context instanceof Activity) {
+                return (AppCompatActivity)context;
+            }
+            context = ((ContextWrapper)context).getBaseContext();
+        }
+        return null;
+    }
 
     @BindingAdapter("app:items")
-    public static void bindList(RecyclerView view, Cursor cursor) {
+    public static void bindList(RecyclerView view, NotesListViewModel viewmodel) {
         //view.setAdapter(new NoteListAdapter(list));
         SimpleCursorRecyclerAdapter adapter = new SimpleCursorRecyclerAdapter();
+        viewmodel.setAdapter(adapter);
         getActivity(view).getSupportLoaderManager()
-                .initLoader(1, null, new MyCursorLoader(view.getContext(), adapter));
+                .initLoader(1, null, viewmodel);
         view.setAdapter(adapter);
     }
 
@@ -68,20 +82,28 @@ public class NotesListViewModel {
         itemAnimator.setRemoveDuration(duration);
         view.setItemAnimator(itemAnimator);
     }
-}
-class MyCursorLoader implements LoaderManager.LoaderCallbacks<Cursor>
-{
-    private Context context;
-    private CursorRecyclerAdapter adapter;
 
-    public MyCursorLoader(Context context, CursorRecyclerAdapter adapter){
-        this.context = context;
+
+
+    public void setAdapter(CursorRecyclerAdapter adapter) {
         this.adapter = adapter;
     }
+
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String SELECTION = "";
+        List<String> selectionArgs = new ArrayList<String>();
+
+        if (args != null && args.containsKey("query")) {
+            SELECTION += " title LIKE ?";
+            selectionArgs.add( "%" + args.getString("query") + "%");
+        }
+
+        final String[] SELECTION_ARGS = new String[selectionArgs.size()];
+        selectionArgs.toArray(SELECTION_ARGS);
+
         return new CursorLoader(context, NotesContentProvider.CONTENT_URI,
-                new String[]{"_id","title","body"}, null, null, null);
+                new String[]{"_id","title","body"}, SELECTION, SELECTION_ARGS, null);
     }
 
     @Override
@@ -93,4 +115,14 @@ class MyCursorLoader implements LoaderManager.LoaderCallbacks<Cursor>
     public void onLoaderReset(Loader<Cursor> loader) {
         adapter.swapCursor(null);
     }
+
+    public void filter(String s) {
+        Bundle bundle = new Bundle();
+        if( !s.isEmpty())
+            bundle.putString("query", s);
+
+        getActivity(context).getSupportLoaderManager().restartLoader(1, bundle, this);
+    }
 }
+
+
